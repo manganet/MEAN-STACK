37cef0c3c57e29419d003226dbb4c39f1384e9ee
manganet <manganet55@gmail.com>
2018-09-21 08:45:16 +0400

    Express Setup & Routes

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const config = require("./config/database");
+// DB Config
+const db = require("./config/database").database;
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+const app = express();
+const users = require("./routes/users");
+// Port Number
+const port = 3000;
+// CORS Middleware
+app.use(cors());
+// Set Static Folder
+app.use(express.static(path.join(__dirname, "public")));
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use("/users", users);
+// Index Route
+app.get("/", (req, res) => {
+  res.send("Invalid Endpoint");
+});
+// Start Server
+app.listen(port, () => {
+  console.log("Server started on port " + port);
+});
---
+++ config/database.js
@@ -0,0 +1,8 @@
+// module.exports = {
+//   database: "mongodb://localhost:27017/meanauth",
+//   secret: "yoursecret"
+// };
+module.exports = {
+  database: "mongodb://moi:moi123456@ds155352.mlab.com:55352/meanauth",
+  secretOrKey: "secret"
+};
