34a6bd4839f14bfd985f5a9fdc857808089c9c38
manganet <manganet55@gmail.com>
2018-09-21 10:06:42 +0400

    User Model & Register

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const cors = require("cors");
 const passport = require("passport");
 const mongoose = require("mongoose");
 const config = require("./config/database");
+
 // DB Config
 const db = require("./config/database").database;
+
 // Connect to MongoDB
 mongoose
   .connect(
@@ -15,21 +17,32 @@ mongoose
   )
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
+
 const app = express();
+
 const users = require("./routes/users");
+
 // Port Number
 const port = 3000;
+
 // CORS Middleware
 app.use(cors());
+
 // Set Static Folder
 app.use(express.static(path.join(__dirname, "public")));
+
 // Body Parser Middleware
 app.use(bodyParser.json());
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+require("./config/passport")(passport);
 app.use("/users", users);
 // Index Route
 app.get("/", (req, res) => {
   res.send("Invalid Endpoint");
 });
+
 // Start Server
 app.listen(port, () => {
   console.log("Server started on port " + port);
